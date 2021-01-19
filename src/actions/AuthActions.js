bf5890c2b9062f132bd7e65d30c9a1ae00ee7f0a
zeritte <zeritte@gmail.com>
2021-01-19 20:54:57 +0300

    add saving credentials to storaeg

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -1,10 +1,13 @@
 import { LOG_IN, LOG_OUT } from "./types";
 import { captureError, sleep } from "../helpers";
+import * as Keychain from "react-native-keychain";
 
-export const login = () => async dispatch => {
+export const login = (username, password) => async dispatch => {
   try {
     dispatch({ type: LOG_IN.request });
     await sleep(3000);
+    // see https://github.com/oblador/react-native-keychain#setgenericpasswordusername-password--accesscontrol-accessible-accessgroup-service-securitylevel-
+    await Keychain.setGenericPassword(username, password);
     dispatch({ type: LOG_IN.success, payload: "some-unique-user-id" });
   } catch (e) {
     dispatch({ type: LOG_IN.failure });
