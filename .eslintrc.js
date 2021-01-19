2c61b9c8cc82d954f88f81db4e40a0fe4d850184
zeritte <zeritte@gmail.com>
2021-01-19 18:28:39 +0300

    setup eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: [
+    "@react-native-community",
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/react",
+    "plugin:react-hooks/recommended"
+  ]
+};
