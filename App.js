1275473790c298b4e85a0751d086f0da6c9664fa
zeritte <zeritte@gmail.com>
2021-01-19 21:31:50 +0300

    have a commit message :)

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { StatusBar } from "react-native";
 import { Provider } from "react-redux";
-import { store } from "./src/helpers";
 import { NavigationService } from "./src/services";
+import { store } from "./src/helpers";
 
 export default function App() {
   return (
