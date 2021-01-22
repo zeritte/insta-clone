ca8b9246aaad49e45de88e909b5c7b8b9321bd8e
zeritte <zeritte@gmail.com>
2021-01-23 01:37:28 +0300

    add geader

---
+++ src/components/ZHeader.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Pressable, Text, StyleSheet } from "react-native";
+
+import { useDispatch } from "react-redux";
+import { logout } from "../actions/AuthActions";
+
+export const ZHeader = React.memo(() => {
+  const dispatch = useDispatch();
+  return (
+    <View style={styles.headerContainer}>
+      <Text>Main Screen</Text>
+      <Pressable onPress={() => dispatch(logout())}>
+        <Text>Log out</Text>
+      </Pressable>
+    </View>
+  );
+});
+
+const styles = StyleSheet.create({
+  headerContainer: { height: 50 }
+});
