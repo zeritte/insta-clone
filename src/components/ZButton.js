084336cf841e1101b9a324543738266aa3639f99
zeritte <zeritte@gmail.com>
2021-01-23 14:38:23 +0300

    improve zbutton

--- src/components/ZButton.js
+++ src/components/ZButton.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { ActivityIndicator, Pressable, Text } from "react-native";
 
-export const ZButton = React.memo(({ onPress, loading }) => (
+export const ZButton = React.memo(({ onPress, loading, label = "Submit", style }) => (
   <Pressable
     style={({ pressed }) => [
       pressed ? { backgroundColor: "rgb(210, 230, 255)" } : {},
-      { height: 50, alignItems: "center", justifyContent: "center" }
+      { height: 50, alignItems: "center", justifyContent: "center" },
+      style
     ]}
     onPress={onPress}
   >
-    {loading ? <ActivityIndicator /> : <Text>Submit</Text>}
+    {loading ? <ActivityIndicator /> : <Text>{label}</Text>}
   </Pressable>
 ));
