6cf87b1539c9d2c93787242e2c44152f7988e352
zeritte <zeritte@gmail.com>
2021-01-23 17:01:08 +0300

    add videos to mockData

--- src/config/mockData.js
+++ src/config/mockData.js
@@ -5,6 +5,12 @@ export const mockData = [
     title: "Hey check this image"
   },
   {
+    key: "5",
+    videoUri:
+      "https://vod-progressive.akamaized.net/exp=1611421564~acl=%2A%2F1307002091.mp4%2A~hmac=024423c57ec6b57b3f6a3e75ede0f88cf1dcad13ecb2e2b0c1b3ea24b6608b08/vimeo-prod-skyfire-std-us/01/1418/13/332093699/1307002091.mp4?download=1&filename=Pexels+Videos+2219383.mp4",
+    title: "Here is a video"
+  },
+  {
     key: "2",
     imageUris: ["https://upload.wikimedia.org/wikipedia/commons/f/ff/Pizigani_1367_Chart_10MB.jpg"],
     title: "What an artifact!"
@@ -16,6 +22,18 @@ export const mockData = [
       "https://7132.com/en/ajax/downloadmedia?media=4887"
     ],
     title: "This is a gallery"
+  },
+  {
+    key: "6",
+    videoUri:
+      "https://vod-progressive.akamaized.net/exp=1611424688~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F408%2F15%2F377041396%2F1574359659.mp4~hmac=efd98fcdab6b010f0e6df7e563fd31a5b2136777d37ef796c91f5093fde18f84/vimeo-prod-skyfire-std-us/01/408/15/377041396/1574359659.mp4?download=1&filename=video.mp4",
+    title: "Cake and icing"
+  },
+  {
+    key: "7",
+    videoUri:
+      "https://vod-progressive.akamaized.net/exp=1611424848~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2516%2F18%2F462584444%2F2048745434.mp4~hmac=c331b7b7b889f773f85e9ceda449e1537bad14e2f537e6b8b45c73062f16b443/vimeo-prod-skyfire-std-us/01/2516/18/462584444/2048745434.mp4?download=1&filename=pexels-cottonbro-5465264.mp4",
+    title: "a vertical video"
   },
   {
     key: "4",
